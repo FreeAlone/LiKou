dbed074cba9fd34daccce1ec2e4a772b7aa0909d
FreeAlone <wys_wangyansong@163.com>
2022-06-19 14:42:32 +0800

    547 三种解法 深度优先搜索 广度优先搜索 并查集

--- src/main/java/com/exam/lk_05/LK_0547.java
+++ src/main/java/com/exam/lk_05/LK_0547.java
@@ -1,8 +1,9 @@
 package com.exam.lk_05;
 
 import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class LK_0547 {
 
@@ -13,14 +14,30 @@ public class LK_0547 {
         int[][] isConnected4 = {{1,1,0,0,0,0,0,1,0,0,0,0,0,0,0},{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,1,0,1,1,0,0,0,0,0,0,0,0},{0,0,0,0,1,0,0,0,0,1,1,0,0,0,0},{0,0,0,1,0,1,0,0,0,0,1,0,0,0,0},{0,0,0,1,0,0,1,0,1,0,0,0,0,1,0},{1,0,0,0,0,0,0,1,1,0,0,0,0,0,0},{0,0,0,0,0,0,1,1,1,0,0,0,0,1,0},{0,0,0,0,1,0,0,0,0,1,0,1,0,0,1},{0,0,0,0,1,1,0,0,0,0,1,1,0,0,0},{0,0,0,0,0,0,0,0,0,1,1,1,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,1,0,1,0,0,0,0,1,0},{0,0,0,0,0,0,0,0,0,1,0,0,0,0,1}};
 
         LK_0547 lk_0547 = new LK_0547();
-//        System.out.println(lk_0547.findCircleNum(isConnected1));
-//        System.out.println(lk_0547.findCircleNum(isConnected2));
+        System.out.println(lk_0547.findCircleNum(isConnected1));
+        System.out.println(lk_0547.findCircleNum(isConnected2));
         System.out.println(lk_0547.findCircleNum(isConnected3));
-//        System.out.println(lk_0547.findCircleNum(isConnected4));
+        System.out.println(lk_0547.findCircleNum(isConnected4));
+        System.out.println();
+        System.out.println(lk_0547.findCircleNumBfs(isConnected1));
+        System.out.println(lk_0547.findCircleNumBfs(isConnected2));
+        System.out.println(lk_0547.findCircleNumBfs(isConnected3));
+        System.out.println(lk_0547.findCircleNumBfs(isConnected4));
+        System.out.println();
+        System.out.println(lk_0547.findCircleNumDfs1(isConnected1));
+        System.out.println(lk_0547.findCircleNumDfs1(isConnected2));
+        System.out.println(lk_0547.findCircleNumDfs1(isConnected3));
+        System.out.println(lk_0547.findCircleNumDfs1(isConnected4));
+        System.out.println();
+        System.out.println(lk_0547.findCircleNumDfs2(isConnected1));
+        System.out.println(lk_0547.findCircleNumDfs2(isConnected2));
+        System.out.println(lk_0547.findCircleNumDfs2(isConnected3));
+        System.out.println(lk_0547.findCircleNumDfs2(isConnected4));
     }
 
     int[] parent;
 
+    // 并查集
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         parent = new int[n];
@@ -37,7 +54,6 @@ public class LK_0547 {
         int p = 0;
         for (int i = 0; i < n; i++) {
             if (i == parent[i]) {
-                System.out.println(parent[i]);
                 p++;
             }
         }
@@ -53,5 +69,77 @@ public class LK_0547 {
 
     public void union(int x, int y) {
         parent[find(y)] = find(x);
+    }
+
+    // 深度优先搜索 递归
+    public int findCircleNumDfs1(int[][] isConnected) {
+        int len = isConnected.length;
+        boolean[] visited = new boolean[len];
+        int ret = 0;
+        for (int i = 0; i < len; i++) {
+            if (!visited[i]) {
+                ret ++;
+                visited[i] = true;
+                dfs(visited, isConnected, len, i);
+            }
+        }
+        return ret;
+    }
+
+    public void dfs(boolean[] visited, int[][] isConnected, int len, int t) {
+        for (int i = 0; i < len; i++) {
+            if (!visited[i] &&  i != t && isConnected[t][i] == 1) {
+                visited[i] = true;
+                dfs(visited, isConnected, len, i);
+            }
+        }
+    }
+
+    // 深度优先搜索 栈
+    public int findCircleNumDfs2(int[][] isConnected) {
+        int len = isConnected.length;
+        boolean[] visited = new boolean[len];
+        int ret = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            if (!visited[i]) {
+                ret++;
+                stack.push(i);
+                while (!stack.isEmpty()) {
+                    Integer k = stack.pop();
+                    visited[k] = true;
+                    for (int j = 0; j < len; j++) {
+                        if (!visited[j] && k != j && isConnected[k][j] == 1) {
+                            stack.push(j);
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    // 广度优先搜索
+    public int findCircleNumBfs(int[][] isConnected) {
+        int len = isConnected.length;
+        boolean[] visited = new boolean[isConnected.length];
+        int ret = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < len; i++) {
+            if (!visited[i]) {
+                queue.offer(i);
+                ret++;
+                while (!queue.isEmpty()) {
+                    Integer temp = queue.poll();
+                    visited[temp] = true;
+                    for (int j = 0; j < len; j++) {
+                        if (isConnected[temp][j] == 1 && !visited[j]) {
+                            queue.offer(j);
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
     }
 }
